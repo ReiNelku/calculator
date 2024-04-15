361acc6708308d4fe86ca6a40ff765c9198e4f22
Rei Nelku <85695388+ReiNelku@users.noreply.github.com>
2024-04-15 17:14:08 +0200

    feat: Create functions to calculate results

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function operate(firstOperand, secondOperand, operator) {
+  switch (operator) {
+    case "+":
+      return add(firstOperand, secondOperand);
+    case "-":
+      return subtract(firstOperand, secondOperand);
+    case "*":
+      return multiply(firstOperand, secondOperand);
+    case "/":
+      return divide(firstOperand, secondOperand);
+  }
+}
